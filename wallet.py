e19ccda9729150efc636bf38f815ec70f042a061
Feng Zhang <71230102+anti31415@users.noreply.github.com>
2021-02-23 19:13:53 -0500

    update

--- wallet.py
+++ wallet.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 
-command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
+command = 'php derive -g --mnemonic="index plug jaguar gesture vibrant scatter laugh above dragon alone memory vapor tree robust deer" --cols=path,address,privkey,pubkey --format=json --coin=ETH'
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 output, err = p.communicate()
